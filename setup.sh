fa149475fac8830f3370d9c7f42ad8c0caa5e626
ds-praveenkumar <praveenkr2208@gmail.com>
2022-10-24 16:26:43 +0530

    update setup.sh with theme

--- setup.sh
+++ setup.sh
@@ -6,3 +6,12 @@ port = $PORT\n\
 enableCORS = false\n\
 \n\
 " > ~/.streamlit/config.toml
+
+echo "\
+[theme]\n\
+primaryColor=\"#2214c7\"\n\
+backgroundColor=\"#F0FFF0\"\n\
+secondaryBackgroundColor=\"#e8eef9\"\n\
+textColor=\"#000000\"\n\
+\n\
+" >> ~/.streamlit/config.toml
